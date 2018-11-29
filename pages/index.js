5bc66c67937dd759c053b81c128a9cd661c35b4a
Matthew Wood <matthewwood@Matthews-MacBook-Pro.local>
2018-11-29 10:29:17 -0600

    init

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Items from '../components/Items';
+import PleaseSignIn from '../components/PleaseSignIn';
+
+const Home = props => (
+  <div>
+    <PleaseSignIn>
+      <Items page={parseFloat(props.query.page) || 1} />
+    </PleaseSignIn>
+  </div>
+);
+
+export default Home;
